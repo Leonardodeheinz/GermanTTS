9ca861429cb6d8e632ae385cd4cbba2a5dc4840b
60773 <s-dhein1@haw-landshut.de>
2025-05-14 22:04:45 +0200

    * adding SinusPositionalEmbedding * rewrite GRN to use newer nnx module instead of linen * add ConvNeXtV2Block

--- model/GermanTTS.py
+++ model/GermanTTS.py
@@ -11,10 +11,13 @@ dt - dimension text
 # neural network libaries
 from ast import Module
 from statistics import mean
+from tokenize import group
 import jax
 import jax.numpy as jnp
-from flax import linen as nn
-import optax
+import matplotlib.pyplot as plt
+import numpy as np
+
+from flax import nnx
 
 # type hinting
 from jaxtyping import ArrayLike
@@ -66,14 +69,13 @@ def xnor(x,y):
 
 # GRN response normalization layer
 
-class GRN(nn.Module):
-    dim:int 
+class GRN(nnx.Module):
+
     
-    def setup(self):
-        self.gamma = self.param("gamma", jnp.zeros(1, 1, self.dim))
-        self.beta = self.param("beta", jnp.zeros(1, 1, self.dim))
-        
-    @nn.compact
+    def __init__(self, dim:int):
+        self.gamma = nnx.Param("gamma", jnp.zeros(1, 1, dim))
+        self.beta = nnx.Param("beta", jnp.zeros(1, 1, dim))
+    
     def __call__(self, x):
         Gx = jnp.linalg.norm(x, "fro", axis=1, keepdim = True)
         Gx_old = jnp.array(Gx)
@@ -81,16 +83,55 @@ class GRN(nn.Module):
         return self.gamma * (x * Nx) + self.beta + x 
         
         
+class ConvNeXtV2Block(nnx.Module):
+
+    
+    def __init__(self, dim:int, intermidiate_dim:int , dilation:int = 1):
+        
+        # depthwise conv
+        padding = (dilation * (7 - 1)) // 2
+        self.dwconv = nnx.Conv(dim, dim, kernel_size = 7, padding = padding, dilation = dilation)
+        self.norm = nnx.LayerNorm(dim, eps = 1e-6) 
+    
+        # pointwise conv
+        self.pwconv1 = nnx.Linear(dim, intermidiate_dim)
+        self.act = nnx.gelu()
+        self.grn = GRN(intermidiate_dim)
+        self.pwconv2 = nnx.Linear(intermidiate_dim, dim)
+    
+
+    def __call__(self, x:jnp.ndarray) -> jnp.ndarray:
+        residual = x
+        x = rearrange(x, "b n d -> b d n")
+        x = self.dwconv(x)
+        x = rearrange(x, "b d n -> b n d")
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.grn(x)
+        x = self.pwconv2(x)
+        return residual + x
+        
+
+class SinusPositonalEmbedding(nnx.Module):
+    def __init__(self, dim:int):
+        self.dim = dim
+        
+    def __call__(self, x:jnp.ndarray, scale = 1000):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = jnp.log(10_000) / half_dim - 1
+        emb = jnp.exp(jnp.arange(half_dim, device = device).float() * -emb)
+        
+        
+        
 
 
-
-
-
-
-
-
-
-
+import jax.numpy as jnp
+import jax
+jax.devices("cpu")
+arr = jnp.array([1,2,3])
+print(arr.device)
 
 
 
